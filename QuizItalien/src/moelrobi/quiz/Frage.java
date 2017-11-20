3c6dfd5a4e272482c0bb19a91e9a9a0b7157231d
Robin M. <moelrobi@gmail.com>
2017-11-20 16:44:17 +0100

    Inital Commit.
    Complete Refactoring from old Code in a New Repo.

---
+++ QuizItalien/src/moelrobi/quiz/Frage.java
@@ -0,0 +1,5 @@
+package moelrobi.quiz;
+
+public class Frage {
+
+}
---
+++ QuizItalien/src/moelrobi/quiz/QuizHelper.java
@@ -0,0 +1,10 @@
+package moelrobi.quiz;
+
+public class QuizHelper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
